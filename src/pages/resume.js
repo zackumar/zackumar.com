915bf52660bebc348c0d5a1f11758e361d9f733a
Zack Umar <zackumar2@gmail.com>
2021-12-26 10:29:01 -0600

    Start resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import Layout from '../components/layout'
+import * as styles from './resume.module.css'
+
+// markup
+const ResumePage = () => {
+    return (
+        <Layout pageTitle="Resume">
+            <h1>Resume</h1>
+        </Layout>
+    )
+}
+
+export default ResumePage

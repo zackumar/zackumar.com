f7ae74f389af65654794a39ab59c7c99eaf1be52
Zack Umar <zackumar2@gmail.com>
2021-12-25 18:06:27 -0600

    New navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,87 +4,87 @@ import styled from 'styled-components'
 import Layout from '../components/layout'
 
 const HeaderText = styled.div`
-  flex: 0 1 auto;
+    flex: 0 1 auto;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 2rem;
-  padding-top: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    padding-top: 4rem;
 
-  h1 {
-    font-size: 3em;
-    max-width: 50rem;
-    text-align: center;
-  }
-
-  @media (min-width: 600px) {
     h1 {
-      font-size: 4em;
+        font-size: 3em;
+        max-width: 50rem;
+        text-align: center;
     }
 
-    margin-bottom: 1rem;
-  }
+    @media (min-width: 600px) {
+        h1 {
+            font-size: 4em;
+        }
+
+        margin-bottom: 1rem;
+    }
 `
 const ContentPane = styled.div`
-  flex: 1 1 auto;
-  background-color: #f4f6f8;
-  padding: 5rem;
+    flex: 1 1 auto;
+    background-color: #f4f6f8;
+    padding: 5rem;
 
-  @media (max-width: 600px) {
-    padding: 2.5rem;
-  }
-
-  section {
-    margin-bottom: 5rem;
-    line-height: 1.5;
-
-    h1 {
-      font-size: 2em;
+    @media (max-width: 600px) {
+        padding: 2.5rem;
     }
 
-    h2 {
-      font-size: 1.75em;
-      margin-bottom: 0;
-      padding-bottom: 0;
-    }
+    section {
+        margin-bottom: 5rem;
+        line-height: 1.5;
 
-    p {
-      font-size: 1.25em;
+        h1 {
+            font-size: 2em;
+        }
+
+        h2 {
+            font-size: 1.75em;
+            margin-bottom: 0;
+            padding-bottom: 0;
+        }
+
+        p {
+            font-size: 1.25em;
+        }
     }
-  }
 `
 
 // markup
 const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home">
-      <HeaderText>
-        <h1>Hi, I'm Zack. I code things sometimes.</h1>
-      </HeaderText>
-      <ContentPane>
-        <section>
-          <h1>Hello, I'm Zack Umar 👋</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus. Dui nunc mattis enim ut tellus
-            elementum sagittis. Sed blandit libero volutpat sed cras ornare arcu dui. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Velit ut tortor pretium viverra
-            suspendisse. Eu feugiat pretium nibh ipsum. Scelerisque varius morbi enim nunc faucibus a pellentesque sit. Dictum sit amet justo donec enim diam vulputate ut. Quam viverra orci sagittis
-            eu volutpat. Id leo in vitae turpis massa. Id consectetur purus ut faucibus pulvinar elementum integer. At ultrices mi tempus imperdiet. Diam donec adipiscing tristique risus nec feugiat.
-            Nibh mauris cursus mattis molestie a iaculis at erat. Aliquet enim tortor at auctor. In arcu cursus euismod quis viverra nibh.
-          </p>
-        </section>
-        <section>
-          <h1>A Handful of Projects 🤲</h1>
-          <h2>Project 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus. Dui nunc mattis enim ut tellus
-            elementum sagittis. Sed blandit libero volutpat sed cras ornare arcu dui. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Velit ut tortor pretium viverra
-            suspendisse.
-          </p>
-        </section>
-      </ContentPane>
-    </Layout>
-  )
+    return (
+        <Layout pageTitle="Home">
+            <HeaderText>
+                <h1>Hi, I'm Zack. I code things sometimes.</h1>
+            </HeaderText>
+            <ContentPane>
+                <section>
+                    <h1>Hello, I'm Zack Umar</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus. Dui nunc mattis enim
+                        ut tellus elementum sagittis. Sed blandit libero volutpat sed cras ornare arcu dui. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Velit ut tortor
+                        pretium viverra suspendisse. Eu feugiat pretium nibh ipsum. Scelerisque varius morbi enim nunc faucibus a pellentesque sit. Dictum sit amet justo donec enim diam vulputate ut.
+                        Quam viverra orci sagittis eu volutpat. Id leo in vitae turpis massa. Id consectetur purus ut faucibus pulvinar elementum integer. At ultrices mi tempus imperdiet. Diam donec
+                        adipiscing tristique risus nec feugiat. Nibh mauris cursus mattis molestie a iaculis at erat. Aliquet enim tortor at auctor. In arcu cursus euismod quis viverra nibh.
+                    </p>
+                </section>
+                <section>
+                    <h1>A Handful of Projects</h1>
+                    <h2>Project 1</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ullamcorper sit amet risus. Dui nunc mattis enim
+                        ut tellus elementum sagittis. Sed blandit libero volutpat sed cras ornare arcu dui. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Velit ut tortor
+                        pretium viverra suspendisse.
+                    </p>
+                </section>
+            </ContentPane>
+        </Layout>
+    )
 }
 
 export default IndexPage

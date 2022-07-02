778a6ed7500aead34f22dccd002f13e295494e7f
Zack Umar <zackumar2@gmail.com>
2022-07-02 17:55:33 -0500

    migrate_card

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,34 @@ import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "../Header";
-import { GlobalStyle, LayoutContent, LayoutWrapper } from "./Layout.styles";
+
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+      scroll-behavior: smooth;
+      margin: 0;
+      padding: 0;
+
+      width: 100%;
+      min-height: 100vh;
+  }
+`;
+
+const LayoutWrapper = styled.div`
+  font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const LayoutContent = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 interface LayoutProps {
   pageTitle: string;

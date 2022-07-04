efb27880def6825e1f26700c0e2c61d07f46bf8d
Zack Umar <zackumar2@gmail.com>
2022-07-04 09:28:19 -0500

    Refactor StyledLink

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -1,6 +1,10 @@
-import { BreakpointName } from "./types"
-import theme from "./themes"
+import { BreakpointName, ColorType } from "./types";
+import theme from "./themes";
 
 export const minWidthBreakpoint = (breakpoint: BreakpointName): string => {
-    return `@media screen and (min-width: ${theme.breakpoints[breakpoint]}px)`
-}
+  return `@media screen and (min-width: ${theme.breakpoints[breakpoint]}px)`;
+};
+
+export const color = (color: string): ColorType => {
+  return theme.colors[color];
+};
